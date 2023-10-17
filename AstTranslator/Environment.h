dd025c4e483121f386769d151f8658fdba00b547
HangShuYH <m18080292938@163.com>
2023-10-17 19:39:02 +0800

    Support Pointer

--- AstTranslator/Environment.h
+++ AstTranslator/Environment.h
@@ -1,6 +1,7 @@
 //==--- tools/clang-check/ClangInterpreter.cpp - Clang Interpreter tool --------------===//
 //===----------------------------------------------------------------------===//
 #include <cassert>
+#include <map>
 #include <stdio.h>
 #include <vector>
 
@@ -22,28 +23,51 @@
 
 using namespace clang;
 
+class Value {
+	union {
+		int intVal;
+		Decl* addrVal;
+	};
+	enum ValueType{IntVal,AddrVal}type;
+public:
+	Value(int val = 0):intVal(val), type(IntVal) {}
+	explicit Value(Decl* val):addrVal(val), type(AddrVal) {}
+	bool isIntVal() const {return type == IntVal;}
+	bool isAddrVal() const {return type == AddrVal;}
+	int getInt() const {return intVal;}
+	Decl* getAddr() const {return addrVal;}
+};
+
 class StackFrame {
    /// StackFrame maps Variable Declaration to Value
    /// Which are either integer or addresses (also represented using an Integer value)
-   std::map<Decl*, int> mVars;
-   std::map<Stmt*, int> mExprs;
+   std::map<Decl*, Value> mVars;
+   std::map<Stmt*, Value> mExprs;
+
+   std::map<Stmt*, Value> mAddrs;
    /// The current stmt
    Stmt * mPC;
 public:
-   StackFrame() : mVars(), mExprs(), mPC() {
+   StackFrame() : mVars(), mExprs(), mPC(), mAddrs() {
    }
    
-   void bindDecl(Decl* decl, int val) {
+   void bindAddr(Stmt* stmt, Value val) {
+	  mAddrs[stmt] = val;
+   }
+   Value getAddr(Stmt* stmt) {
+	  return mAddrs[stmt];
+   }
+   void bindDecl(Decl* decl, Value val) {
       mVars[decl] = val;
    }    
-   int getDeclVal(Decl * decl) {
+   Value getDeclVal(Decl * decl) {
       assert (mVars.find(decl) != mVars.end());
       return mVars.find(decl)->second;
    }
-   void bindStmt(Stmt * stmt, int val) {
+   void bindStmt(Stmt * stmt, Value val) {
 	   mExprs[stmt] = val;
    }
-   int getStmtVal(Stmt * stmt, const ASTContext& Context) {
+   Value getStmtVal(Stmt * stmt, const ASTContext& Context) {
 	   if (Expr* expr = llvm::dyn_cast<Expr>(stmt)) {
 			Expr::EvalResult result;
 			if (expr->EvaluateAsInt(result, Context)) {
@@ -71,6 +95,7 @@ public:
    int get(int addr);
 };
 */
+
 
 class Environment {
    std::vector<StackFrame> mStack;
@@ -113,16 +138,19 @@ public:
 	    Expr * right = bop->getRHS();
 
 	    if (bop->isAssignmentOp()) {
-		   int val = mStack.back().getStmtVal(right, mContext);
+		   Value val = mStack.back().getStmtVal(right, mContext);
 		   mStack.back().bindStmt(left, val);
 		   if (DeclRefExpr * declexpr = dyn_cast<DeclRefExpr>(left)) {
 			   Decl * decl = declexpr->getFoundDecl();
 			   mStack.back().bindDecl(decl, val);
+		   } else {
+			   Decl* decl = mStack.back().getAddr(left).getAddr();
+			   mStack.back().bindDecl(decl, val);
 		   }
 		   return;
 	    }
-		int leftVal = mStack.back().getStmtVal(left, mContext);
-		int rightVal = mStack.back().getStmtVal(right, mContext);
+		int leftVal = mStack.back().getStmtVal(left, mContext).getInt();
+		int rightVal = mStack.back().getStmtVal(right, mContext).getInt();
 		int resultVal;
 		switch (bop->getOpcode()) {
 			case clang::BO_Add:
@@ -173,19 +201,19 @@ public:
    
    void declref(DeclRefExpr * declref) {
 	   mStack.back().setPC(declref);
-	   if (declref->getType()->isIntegerType()) {
+	   if (!declref->getType()->isFunctionType()) {
 		   Decl* decl = declref->getFoundDecl();
 
-		   int val = mStack.back().getDeclVal(decl);
+		   Value val = mStack.back().getDeclVal(decl);
 		   mStack.back().bindStmt(declref, val);
 	   }
    }
 
    void cast(CastExpr * castexpr) {
 	   mStack.back().setPC(castexpr);
-	   if (castexpr->getType()->isIntegerType()) {
+	   if (!castexpr->getType()->isFunctionPointerType()) {
 		   Expr * expr = castexpr->getSubExpr();
-		   int val = mStack.back().getStmtVal(expr, mContext);
+		   Value val = mStack.back().getStmtVal(expr, mContext);
 		   mStack.back().bindStmt(castexpr, val );
 	   }
    }
@@ -202,7 +230,7 @@ public:
 		  mStack.back().bindStmt(callexpr, val);
 	   } else if (callee == mOutput) {
 		   Expr * decl = callexpr->getArg(0);
-		   val = mStack.back().getStmtVal(decl, mContext);
+		   val = mStack.back().getStmtVal(decl, mContext).getInt();
 		   llvm::errs() << val;
 	   } else {
 		   /// You could add your code here for Function call Return
@@ -213,13 +241,33 @@ public:
    void unary(UnaryOperator* unaryOperator) {
 	   mStack.back().setPC(unaryOperator);
 	   Expr* subExpr = unaryOperator->getSubExpr();
-	   return mStack.back().bindStmt(unaryOperator, 
-	         -mStack.back().getStmtVal(subExpr,  mContext));
+	   switch (unaryOperator->getOpcode()) {
+			default:
+				llvm::errs() << "UnSupported UnaryOpcode!\n";
+				break;
+			case clang::UO_Minus:
+				mStack.back().bindStmt(unaryOperator, 
+	         -mStack.back().getStmtVal(subExpr,  mContext).getInt());
+			 	break;
+			case clang::UO_AddrOf:
+				if (DeclRefExpr * declexpr = dyn_cast<DeclRefExpr>(subExpr)) {
+			  		Decl * decl = declexpr->getFoundDecl();
+			   		mStack.back().bindStmt(unaryOperator, Value(decl));
+		   		}
+				break;
+			case clang::UO_Deref:
+				mStack.back().bindStmt(unaryOperator, 
+					mStack.back().getDeclVal(
+						mStack.back().getStmtVal(subExpr, mContext).getAddr())
+				);
+				mStack.back().bindAddr(unaryOperator, mStack.back().getStmtVal(subExpr, mContext));
+				break;
+	   }
    }
 
    bool getCond(Stmt* cond) {
 	  mStack.back().setPC(cond);
-	  return mStack.back().getStmtVal(cond, mContext) != 0;
+	  return mStack.back().getStmtVal(cond, mContext).getInt() != 0;
    }
 
 //    void arraySubscriptExpr(ArraySubscriptExpr* arraySubscriptExpr) {
